dd28a6a6e7e76659382892dfed58ce7a9919a6c5
Sankalp <your.email@example.com>
2025-09-23 23:14:15 +0530

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,33 @@
-import { FC, lazy, Suspense } from "react";
+import { FC } from "react";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Skills from "./components/Skills";
+import Certificates from "./components/Certificates";
+import Projects from "./components/Projects";
+import Experience from "./components/Experience";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-
-// Lazy load the heavy components
-const Projects = lazy(() => import("./components/Projects"));
-const Certificates = lazy(() => import("./components/Certificates"));
-const Experience = lazy(() => import("./components/Experience"));
+import ParallaxBackground from "./components/ParallaxBackground";
+import CustomCursor from "./components/CustomCursor";
 
 const App: FC = () => {
-  // const [isMobile, setIsMobile] = useState(false);
-
-  // useEffect(() => {
-  //   const handleResize = () => {
-  //     setIsMobile(window.innerWidth < 768); // You can adjust this breakpoint
-  //   };
-
-  //   handleResize(); // Set initial state
-  //   window.addEventListener("resize", handleResize);
-
-  //   return () => window.removeEventListener("resize", handleResize);
-  // }, []);
-
   return (
     <ThemeProvider>
-      <div className="relative min-h-screen bg-white dark:bg-dark-900 text-gray-900 dark:text-white transition-colors duration-300">
+      <div className="relative min-h-screen bg-white dark:bg-dark-900 text-gray-900 dark:text-white transition-colors duration-300 ">
+        <div className="hidden sm:block">
+          <CustomCursor />
+          <ParallaxBackground />
+        </div>
         <Header />
         <main className="relative z-10">
           <Hero />
           <About />
           <Skills />
-          {/* Use Suspense for lazy loaded components */}
-          <Suspense fallback={<div>Loading...</div>}>
-            <Certificates />
-            <Experience />
-            <Projects />
-          </Suspense>
+          <Certificates />
+          <Experience />
+          <Projects />
           <Contact />
         </main>
         <Footer />
